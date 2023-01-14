ceabfc2c8b72f94bb6399ad12f14ab4194a34388
Sagar Chauhan <coolsagar005@gmail.com>
2023-01-14 16:47:21 +0530

    stable working with single popup only

--- background.js
+++ background.js
@@ -1,30 +1,38 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        id: "format",
-        title: 'JSON Formatter',
-        contexts: ['selection'],
-    });
-
-    chrome.contextMenus.onClicked.addListener(function(info, tab) {
-        console.log("context menu was clicked");
-        if (info.menuItemId === "format") {
-            if (info.selectionText) {
-                let json = encodeURI(info.selectionText);
-                chrome.scripting.executeScript({
-                    target: {tabId: tab.id},
-                    function: generateFormatUrl,
-                    args: [json],
-                });
-            }
-        }else{
-            console.log("something went wrong");
-        }
-    })
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete' && tab.active) {
+        chrome.contextMenus.removeAll(function() {
+            chrome.contextMenus.create({
+                id: "format",
+                title: 'JSON Formatter',
+                contexts: ['selection']
+            });
+        });
+    }
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    console.log("context menu was clicked");
+    if (info.menuItemId === "format") {
+        if (info.selectionText) {
+            let json = encodeURI(info.selectionText);
+            chrome.scripting.executeScript({
+                target: {tabId: tab.id},
+                function: generateFormatUrl,
+                args: [json],
+            });
+        }
+    }else{
+        console.log("something went wrong");
+    }
+})
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     initContextMenu();
+// });
 
 function generateFormatUrl(json) {
     // check if the url is a medium article
     let new_url = "https://jsonformatter.curiousconcept.com/?data="+json+"&process=true";
     console.log("new_url", new_url);
-    window.open(new_url, "_blank");
+   window.open(new_url, "_blank");
 }
